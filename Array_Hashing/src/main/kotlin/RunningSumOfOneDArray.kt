76d2e223a7d3fef26ca35a5aa4ac62d918251113
Gobind Narayan Singh <gobind.dli@hotmail.com>
2022-07-17 23:13:21 +0530

    modify

--- Array_Hashing/src/main/kotlin/RunningSumOfOneDArray.kt
+++ Array_Hashing/src/main/kotlin/RunningSumOfOneDArray.kt
@@ -2,12 +2,9 @@ class RunningSumOfOneDArray {
 
     fun runningSum(nums: IntArray): IntArray {
         //var runningSumArray: IntArray = IntArray(nums.size)
-        for (i in nums.indices) {
-            if (i == 0) {
-                continue
-            } else {
-                nums[i] += nums[i-1]
-            }
+        for (i in 1 until nums.size) {
+            nums[i] += nums[i-1]
+
         }
         return nums
     }
